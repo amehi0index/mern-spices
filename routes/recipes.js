a31b01b55d02cd48d0b95f102709c6894fe21f63
Amelia Hill <amehi4yus@gmail.com>
2022-07-25 08:21:27 -0700

    Img tag

--- routes/recipes.js
+++ routes/recipes.js
@@ -6,7 +6,7 @@ const apicache = require('apicache')
 const url = require('url')
 const rateLimit = require('express-rate-limit')
 
-const API_BASE_URL  = process.env.API_BASE_URL
+const API_BASE_URL = process.env.API_BASE_URL
 let cache = apicache.middleware
 
 const limiter = rateLimit({
@@ -17,20 +17,19 @@ const limiter = rateLimit({
 router.get('/', limiter, async (req, res) => {  //cache('2 minutes')
 
     try {
-
         const params = new URLSearchParams({
-            app_id:  process.env.API_ID_VALUE,
+            app_id: process.env.API_ID_VALUE,
             app_key: process.env.API_KEY_VALUE,
             ...url.parse(req.url, true).query
-            
+
         });
-   
-        const apiRes = await needle('get', `${API_BASE_URL}?${params}`)  
+
+        const apiRes = await needle('get', `${API_BASE_URL}?${params}`)
         const data = apiRes.body
 
         res.status(200).json(data)
     } catch (error) {
-        res.status(500).json({error})
+        res.status(500).json({ error })
     }
 })
 
