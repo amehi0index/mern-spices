58dab1248f138853ccbdff830fc699ace96b3697
Amelia Hill <amehi4yus@gmail.com>
2022-09-16 19:29:06 -0700

    Formatting

--- client/src/context/types.js
+++ client/src/context/types.js
@@ -8,11 +8,11 @@ export const DELETE_HERB = 'DELETE_HERB';
 export const HERB_ERROR = 'HERB_ERROR';
 
 export const SET_CURRENT = 'SET_CURRENT';
-export const CLEAR_CURRENT = 'CLEAR_CURRENT';
+export const CLEAR_CURRENT = 'CLEAR_CURRENT'
 
-export const FILTER_HERBS = 'FILTER_HERBS';
-export const CLEAR_HERBS = 'CLEAR_HERBS';
-export const CLEAR_FILTER = 'CLEAR_FILTER';
+export const FILTER_HERBS = 'FILTER_HERBS'
+export const CLEAR_HERBS = 'CLEAR_HERBS'
+export const CLEAR_FILTER = 'CLEAR_FILTER'
 
-export const SET_ALERT ='SET_ALERT ';
-export const REMOVE_ALERT ='REMOVE_ALERT';
+export const SET_ALERT ='SET_ALERT '
+export const REMOVE_ALERT ='REMOVE_ALERT'
